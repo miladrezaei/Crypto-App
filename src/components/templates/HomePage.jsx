091ad7b005b0853ef17670d2e7af092b8e966457
Milad jahed <miladrezaei522@gmail.com>
2025-04-04 15:08:39 +0330

    add currency

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -3,23 +3,26 @@ import TableCoin from '../modules/TableCoin';
 
 import { getCoinList } from '../../services/CryptoApi';
 import Pagination from '../modules/Pagination';
+import Search from '../modules/Search';
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsloading] = useState(true);
   const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState('usd');
   useEffect(() => {
     setIsloading(true);
     const getData = async () => {
-      const res = await fetch(getCoinList(page));
+      const res = await fetch(getCoinList(page, currency));
       const json = await res.json();
       setCoins(json);
       setIsloading(false);
     };
     getData();
-  }, [page]);
+  }, [page, currency]);
   return (
     <div>
+      <Search currency={currency} setCurrency={setCurrency} />
       <TableCoin coins={coins} isLoading={isLoading} />
       <Pagination page={page} setPage={setPage} />
     </div>
