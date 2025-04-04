7cfebd61afdd6e474d71875cb8303e287b838075
Milad jahed <miladrezaei522@gmail.com>
2025-04-04 13:45:40 +0330

    creat Pagination

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -2,21 +2,25 @@ import React, { useEffect, useState } from 'react';
 import TableCoin from '../modules/TableCoin';
 
 import { getCoinList } from '../../services/CryptoApi';
+import Pagination from '../modules/Pagination';
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsloading] = useState(true);
+  const [page, setPage] = useState(1);
   useEffect(() => {
+    setIsloading(true);
     const getData = async () => {
-      const res = await fetch(getCoinList());
+      const res = await fetch(getCoinList(page));
       const json = await res.json();
       setCoins(json);
       setIsloading(false);
     };
     getData();
-  }, []);
+  }, [page]);
   return (
     <div>
+      <Pagination page={page} setPage={setPage} />
       <TableCoin coins={coins} isLoading={isLoading} />
     </div>
   );
